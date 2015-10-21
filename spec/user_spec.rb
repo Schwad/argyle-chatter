7a84e74391278bc0cd3468040bbf90c1c9f88b14
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-10-21 15:02:41 +0100

    clears out pre factory girl clutter

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,65 +2,32 @@ require 'rails_helper'
 
  describe User do
 
-  let(:user){build(:user)}
-    it "is valid with standard attributes (factory)" do
-      expect(user).to be_valid
-    end
+    let(:user){build(:user)}
+    let(:admin){build(:admin)}
+
+    describe "attributes" do
+      context "with one user" do
+        it "is valid with standard attributes (factory)" do
+          expect(user).to be_valid
+        end
+        it "is not an admin on default" do
+          expect(user.is_admin).to eq(false)
+        end
+        it "is an admin with an appropriate attribute" do
+          expect(admin.is_admin).to eq(true)
+        end
+      end
+      context "with multiple users" do
 
 
-    before :each do
-      User.destroy_all
-      Post.destroy_all
-      Comment.destroy_all
-      @user = User.create(
-        id: 1,
-        email: "schwad@gmail.com",
-        password: "argyle chat"
-        )
-      @post1 = Post.create(
-        id: 1,
-        title: "well hello there",
-        body: "this is awkward",
-        user_id: 1
-        )
-      @post2 = Post.create(
-        id: 2,
-        title: "well hello there",
-        body: "this is awkward",
-        user_id: 1
-        )
-      @comment1 = Comment.create(
-        id: 1,
-        body: "That is a wild post",
-        user_id: 1,
-        post_id: 2
-        )
-      @comment2 = Comment.create(
-        id: 2,
-        body: "That is a wild comment",
-        user_id: 1,
-        post_id: 1
-        )
-      @comment3 = Comment.create(
-        id: 3,
-        body: "No it's not",
-        user_id: 1,
-        post_id: 2
-        )
+      end
     end
 
     it "is valid with email and password" do
-      user = User.new(
-        email: "hello@gmail.com",
-        password: "argyle chat",
-      )
       expect(user).to be_valid
     end
 
     it "is invalid without email" do
-      user = User.new(
-        password: "argyle chat"
-      )
       expect(user).to_not be_valid
     end
 
