6fcef5b18b27af1f23e28c38b5ea153386b6d25b
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-10-22 15:12:42 +0100

    changes has one to belongs to for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,4 @@
 class Comment < ActiveRecord::Base
-  has_one :post
-  has_one :user
+  belongs_to :post
+  belongs_to :user
 end
--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -1,9 +1,25 @@
 require "rails_helper"
 
+
 describe Comment do
-  it "is valid with a user"
-  it "is valid with a post"
-  it "is invalid without a post"
-  it "is invalid without a user"
-  it "only belongs to one user and one post"
+
+  let(:comment){build(:comment)}
+  let(:user){build(:user)}
+  let(:post){build(:post)}
+
+  describe "attributes" do
+    context "with one comment" do
+        it "is valid with a user" do
+          user.save!
+          comment.save!
+          expect(comment.user).to be_valid
+        end
+        it "is valid with a post"
+        it "is invalid without a post"
+        it "is invalid without a user"
+        it "only belongs to one user and one post"
+
+    end
+  end
+
 end
