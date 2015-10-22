84567afcb80682b5f614f67a966f33a0d899ac4a
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-10-22 15:15:07 +0100

    post now belongs to user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  has_one :user
+  belongs_to :user
   has_many :comments, dependent: :destroy
   validates :user_id, presence: true
   validates :title, :length => { :in => 8..200 }
