532706ab9efb9b82533da5534c1ef27a995a00ae
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-10-22 15:29:38 +0100

    creates model tests for comment and requires post for comment to be valid

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -9,17 +9,25 @@ describe Comment do
 
   describe "attributes" do
     context "with one comment" do
-        it "is valid with a user" do
-          user.save!
-          comment.save!
-          expect(comment.user).to be_valid
-        end
-        it "is valid with a post"
-        it "is invalid without a post"
-        it "is invalid without a user"
-        it "only belongs to one user and one post"
 
+        before do
+          user.comments = create_list(:comment, 2)
+          user.save!
+        end
+
+        it "is valid with a user" do
+          expect(user.comments.count).to eq(2)
+        end
+
+        it "is invalid without a post" do
+          new_comment = build(:comment, :post_id => nil)
+          expect(new_comment).to_not be_valid
+        end
+
+        it "is invalid without a user" do
+          new_comment = build(:comment, :user_id => nil)
+          expect(new_comment).to_not be_valid
+        end
     end
   end
-
 end
