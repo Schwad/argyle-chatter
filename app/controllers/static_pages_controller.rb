8714cdc87b715fbc6ff9a12fb2c20a386c48c81f
Nick Schwaderer <nicholas.schwaderer@gmail.com>
2015-10-29 16:24:03 +0000

    includes navbar partial

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,12 @@
 class StaticPagesController < ApplicationController
   def index
+
+    if current_user
+      @user = current_user
+    else
+      @user = nil
+    end
+
     @posts = Post.all
   end
 end
